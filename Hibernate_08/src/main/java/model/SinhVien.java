189ba6f27b6160d2b55c079c60ab83bedbca8e65
Yuh <huypham3188@gmail.com>
2024-07-06 15:26:01 +0700

    inheritance

---
+++ Hibernate_08/src/main/java/model/SinhVien.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class SinhVien extends ConNguoi{
+	private double diemTrungBinh;
+
+	public SinhVien() {
+		
+	}
+
+	public SinhVien(String id, String hoVaTen, Date ngaySinh, double diemTrungBinh) {
+		super(id, hoVaTen, ngaySinh);
+		this.diemTrungBinh = diemTrungBinh;
+	}
+
+	public double getDiemTrungBinh() {
+		return diemTrungBinh;
+	}
+
+	public void setDiemTrungBinh(double diemTrungBinh) {
+		this.diemTrungBinh = diemTrungBinh;
+	}
+	
+}
